e80b41b6e54d4e9a733577e58dad2b540e6b485a
anibalcoder <144500768+anibalcoder@users.noreply.github.com>
2024-07-23 12:41:03 -0500

    feat(movieGenres): inject movieGenres from JavaScript using CSS Modules

---
+++ src/components/movieGenres/movieGenres.js
@@ -0,0 +1,34 @@
+import Handlebars from 'handlebars';
+import getTmdbData from '../../api/getTmdbData.js';
+import './category.css';
+import styles from './movieGenres.module.css';
+
+async function createMovieGenres({ title, urlApi }) {
+  try {
+    const section = document.createElement('section');
+    const data = await getTmdbData(urlApi);
+
+    const template = Handlebars.compile(`
+      <div class="container">
+        {{#if title}}
+          <h2 class="{{styles.movieGenre__title}}">{{title}}</h2>
+        {{/if}}
+
+        <article class="{{styles.movieGenre__list}}">
+          {{#each genres}}
+            <div class="{{../styles.movieGenre__item}}">
+              <h3 class="{{../styles.movieGenre__category}}" id="category{{this.id}}">{{this.name}}</h3>
+            </div>
+          {{/each}}
+        </article>
+      </div>
+    `);
+
+    section.innerHTML = template({ title, styles, genres:data.genres });
+    return section;
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export default createMovieGenres;
--- src/main.js
+++ src/main.js
@@ -2,14 +2,21 @@ import './styles/global.css';
 import './styles/vars.css';
 import createHeader from './components/header/header.js';
 import createMoviePreview from './components/moviePreview/moviePreview.js';
+import createMovieGenres from './components/movieGenres/movieGenres.js';
+const API_BASE = import.meta.env.VITE_API_BASE;
 
 const APP = document.getElementById('app');
 APP.appendChild(createHeader());
 
-const API_BASE = import.meta.env.VITE_API_BASE;
 const moviePreview = await createMoviePreview({
   title: 'Tendencias', 
   button: true, 
   urlApi: `${API_BASE}/trending/all/day?language=es-ES`
 })
 APP.appendChild(moviePreview);
+
+const movieGenres = await createMovieGenres({
+  title: 'Categorías',
+  urlApi: `${API_BASE}/genre/movie/list?language=es-ES`
+})
+APP.appendChild(movieGenres);
